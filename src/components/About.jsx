0d80151f9b2cf4da6cb8dfed3f07c8a390f23092
tamimkhan82 <hmdtamimkhan@gmail.com>
2024-12-31 21:07:01 +0600

    about section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,38 @@
 
+import AboutImg from '../assets/profile.png'
+import { FaChevronRight } from 'react-icons/fa'
 
-export default function About() {
+const About = () => {
   return (
-    <div>
-
+    <div id='about' className='py-20 bg-gray-900 z-50 text-gray-300'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <h2 className='text-4xl md:text-5xl font-bold mb-11 text-center'>About Me</h2>
+        <div className='flex flex-col md:flex-row gap-20 items-center'>
+            {/* img section */}
+            <div className='relative border hidden md:block border-blue-600 p-1 rounded-full bg-blue-500 shadow-[0px_0px_20px_10px_rgba(0,0,0,0.3)] shadow-blue-500'>
+                <img src={AboutImg} alt="" className='w-[300px] md:w-[1500px] rounded-full border-blue-600 border shadow-blue-500 '/>
+            </div>
+            {/* paragraph section */}
+            <div className='p-5 md:p-10 bg-gray-950 rounded-md shadow-[0px_0px_20px_10px_rgba(0,0,0,0.3)] shadow-blue-500'>
+                <p className='text-lg leading-7 mb-6'>Hi! I’m Tamim Khan, a modern and creative Frontend Developer specializing in React.js, Next.js, and Tailwind CSS. With a passion for crafting clean, elegant, and user-friendly web interfaces, I bring ideas to life by combining technical expertise with innovative design.</p>
+                <p className='text-lg leading-7 mb-6'>Coming from a non-IT background, I’ve embraced every challenge as an opportunity to grow and evolve. My journey into web development has been fueled by determination and a strong desire to learn, making me adaptable and highly resourceful in solving problems and implementing cutting-edge solutions.</p>
+                <p className='text-lg leading-7 '>I take pride in staying updated with the latest trends in web development, ensuring that every project I work on meets modern standards and exceeds expectations. Let’s work together to create something amazing!</p>
+                <div className='mt-5'>
+                    <h1 className='font-bold text-2xl mb-5 text-blue-400'>My Skills</h1>
+                    <div className='flex flex-wrap gap-4 mb-5 text-sm'>
+                        <p className='bg-gray-900 cursor-pointer text-gray-300 px-2 py-1 rounded-full'>React</p>
+                        <p className='bg-gray-900 cursor-pointer text-gray-300 px-2 py-1 rounded-full'>Next.js</p>
+                        <p className='bg-gray-900 cursor-pointer text-gray-300 px-2 py-1 rounded-full'>Tailwind Css</p>
+                        <p className='bg-gray-900 cursor-pointer text-gray-300 px-2 py-1 rounded-full'>Redux</p>
+                        <p className='bg-gray-900 cursor-pointer text-gray-300 px-2 py-1 rounded-full'>Github</p>
+                    </div>
+                    <button className='px-3 py-1 rounded-md bg-blue-500 text-white flex items-center gap-1'>Contact<FaChevronRight/></button>
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
+
+export default About
